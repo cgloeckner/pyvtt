709a131f611189277f5cd2171f25154081b5bceb
cgloeckner <cgloeckner@freenet.de>
2021-03-25 15:59:56 +0100

    Feature: migration tool --import

--- migration.py
+++ migration.py
@@ -7,17 +7,30 @@ Copyright (c) 2020-2021 Christian Glöckner
 License: MIT (see LICENSE for details)
 """
 
-import json, sys, pathlib
+import json, sys, os
 
 from engine import Engine
+from utils import PathApi
 
 
 __author__ = 'Christian Glöckner'
 __licence__ = 'MIT'
 
 
+def rename_backup(fname):
+    i = 1
+    while True:
+        target = str(fname) + '.{0}'.format(i)
+        if os.path.exists(target):
+            i += 1
+        else:
+            break
+    os.rename(fname, target)
+
+
 if __name__ == '__main__':
     if '--export' in sys.argv:
+        # boot engine
         engine = Engine(argv=['--quiet'])
         print('{0} migration export started.'.format(engine.title))
 
@@ -28,7 +41,29 @@ if __name__ == '__main__':
         p = engine.paths.root / 'export.json'
         with open(p, 'w') as h:
             h.write(json.dumps(data, indent=4))
+        
+        print('Export finished.')
 
     else:
-        print('NOT IMPLEMENTED YET')
+        # rename all databases to like gm.db.1 etc.
+        print('Rename database files')
+        paths = PathApi(appname='pyvtt')
+        rename_backup(paths.getMainDatabasePath())
+        for gmname in os.listdir(paths.getGmsPath()):
+            fname = paths.getDatabasePath(gmname)
+            rename_backup(fname)
+
+        # boot engine (create tables etc.)  
+        engine = Engine(argv=['--quiet'])
+        print('{0} migration export started.'.format(engine.title))
+
+        # load json from file 
+        p = engine.paths.root / 'export.json'
+        with open(p, 'r') as h:
+            data = json.loads(h.read())
+
+        # import data from dict
+        engine.loadFromDict(data)
+        
+        print('Import finished.')
 
