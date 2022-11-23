c776205a02052ce3bb23c37a335fe7010a6d396e
Scott Merrill <skippy@skippy.net>
2022-11-23 08:55:01 -0500

    actually build in the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 TAG="${1:-latest}"
 SHA=$(git rev-parse HEAD)
 echo ${SHA} > sha.txt
-echo docker build -t pyvtt:${SHA} .
-echo docker tag pyvtt:${SHA} pyvtt:${TAG}
+docker build -t pyvtt:${SHA} .
+docker tag pyvtt:${SHA} pyvtt:${TAG}
