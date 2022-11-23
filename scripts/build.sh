bf30be99e63ef85e340f398b771344d06287edcc
Scott Merrill <skippy@skippy.net>
2022-11-23 15:37:37 -0500

    s/SHORT/SHA/

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TAG="${1:-latest}"
 SHA=$(git rev-parse --short HEAD)
-echo ${SHORT} > sha.txt
+echo ${SHA} > sha.txt
 docker build -t pyvtt:${SHA} .
 docker tag pyvtt:${SHA} pyvtt:${TAG}
