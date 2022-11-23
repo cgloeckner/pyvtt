638cee468309288be7df6d765cf93b0d1415c4d5
Scott Merrill <skippy@skippy.net>
2022-11-23 09:47:11 -0500

    rework run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-if [ $1 = "-h" ]; then
-  echo "build.sh [TAG] [PORT] [MOUNT]"
-  exit 0;
-fi
+PORT="8080"
+VOLUME="/opt/pyvtt/prod"
 
-TAG="${1:-latest}"
-PORT="${2:-8080}"
-MOUNT="${3:-/opt/pyvtt/prod}"
+usage() { echo "build.sh [-p PORT] [-v DIRECTORY] TAG [DOCKER COMMANDS]"; exit 0; }
+
+[ $# -eq 0 ] && usage
+
+while getopts ":hp:v:" arg; do
+  case "${arg}" in
+    p)
+      PORT=${OPTARG}
+      ;;
+    v)
+      VOLUME=${OPTARG}
+      ;;
+    h | *)
+      usage
+      exit 0
+      ;;
+  esac
+done
+# remove parsed optional arguments
+shift $((OPTIND-1))
+# get the tag, and remove it from arguments
+TAG=$1; shift;
+
 docker stop icvtt-${TAG}
 docker rm icvtt-${TAG}
-docker run -d -p ${PORT}:${PORT} -v ${MOUNT}:${MOUNT} --restart=on-failure --name icvtt-${TAG} pyvtt:${TAG}
+docker run -d -p ${PORT}:${PORT} -v ${VOLUME}:${VOLUME} --restart=on-failure --name icvtt-${TAG} pyvtt:${TAG} "$@"
