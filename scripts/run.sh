4e71fca7d95c41080dd4590c5bb63e244f60e2c4
Scott Merrill <skippy@skippy.net>
2022-11-23 08:58:44 -0500

    transposed a quote mark.

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ fi
 
 TAG="${1:-latest}"
 PORT="${2:-8080}"
-MOUNT="${3:-/opt/pyvtt/prod"}
+MOUNT="${3:-/opt/pyvtt/prod}"
 docker stop icvtt-${TAG}
 docker rm icvtt-${TAG}
 docker run -d -p ${PORT}:${PORT} -v ${MOUNT}:${MOUNT} --restart=on-failure --name icvtt-${TAG} pyvtt:${TAG}
