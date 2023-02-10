11c10794909e4d8fc1df80cc9e3fac1bcfa0a54f
cgloeckner <cgloeckner@freenet.de>
2023-02-10 18:58:36 +0100

    Feature: export game admin tool

---
+++ export_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3 
+# -*- coding: utf-8 -*- 
+"""
+https://github.com/cgloeckner/pyvtt/
+
+Copyright (c) 2020-2021 Christian Glöckner
+License: MIT (see LICENSE for details)
+"""
+
+import sys
+
+import engine
+from orm import db_session
+
+__author__ = 'Christian Glöckner'
+__licence__ = 'MIT'
+
+
+if __name__ == '__main__':
+    e = engine.Engine(sys.argv)
+
+    gm_url   = sys.argv[1]
+    game_url = sys.argv[2]
+
+    gm_cache = e.cache.getFromUrl(gm_url)
+
+    with db_session:
+        game = gm_cache.db.Game.select(lambda g: g.url == game_url).first()
+
+        zip_file, zip_path = game.toZip()
+
+        print(gm_url, '/', game_url)
+        print(zip_path / zip_file)
