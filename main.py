ebfc6d6a6cd1a0007e506ae529f40a8ae5157348
Scott Merrill <skippy@skippy.net>
2025-01-20 13:00:59 -0500

    remove shard registration from main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
         engine = Engine(argv=argv)
         routes.register_gm(engine)
         routes.register_player(engine)
-        routes.register_api(engine)
         routes.register_resources(engine)
         routes.register_error(engine)
 
